5c68e0fad2e08e0196ebd529da2969b5b5e47280
Nithin Chowdary Vakalapudi <97021251+nithinchowdary2532@users.noreply.github.com>
2024-04-20 22:53:54 +0530

    Update reject_user.php

--- php/reject_user.php
+++ php/reject_user.php
@@ -4,7 +4,7 @@ include 'connect.php';
 if (isset($_POST['reject'])) {
     $user_id = $_POST['user_id'];
 
-    $update_user = $conn->prepare("UPDATE `users` SET approved = 2 WHERE id = ?");
+    $update_user = $conn->prepare("UPDATE `users` SET approved = 2 WHERE user_id = ?");
     $update_user->execute([$user_id]);
 
     if ($update_user->rowCount() > 0) {
